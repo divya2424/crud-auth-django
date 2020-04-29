aa4590b0385a9ff542a3ec5dbafd8575dfd97aae
Divya Jain <divya2401@bitbucket.org>
2020-04-29 19:21:44 +0530

    added pagination to handle n number of shipments

--- api/shipment/views.py
+++ api/shipment/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404, render
 from rest_framework.views import APIView
+from rest_framework import generics, pagination
 from .models import ShipmentRetailer, ShipmentItem
 from .serializers import ShipmentItemSerializer, ShipmentRetailerSerializer
 from django.conf import settings
@@ -9,59 +10,26 @@ import json
 from .tasks import fetchShipment,immediate_load
 
 
+# from django.core.paginator import Paginator
+ # paginator = Paginator(shipment, 3)
+# page = request.GET.get('page')
+# posts = paginator.get_page(page)
+# print('post',posts.paginator.num_pages)
+# arr = []
+
+# for post in posts:
+#     obj = {}
+#     print('post111',post)
+#     obj['id'] = post.id
+#     arr.append(obj)
+                    
 
 
-def getShipment():
-    shipments = ShipmentRetailer.objects.all()
-    shipmentArr = []           
-    if len(shipments) > 0:                
-        for shipment in shipments:
-            shipmentObj = {}
-            shipmentObj['id'] = shipment.id
-            shipmentObj['shipment_id'] = shipment.shipment_id
-            shipmentObj['shipment_date'] = shipment.shipment_date
-            shipmentObj['transport_id'] = shipment.transport_id
-            shipmentArr.append(shipmentObj)
-
-    return shipmentArr
-    
-class ShipmentView(APIView):
-    def get(self, request, *args, **kwargs):
-        pk = self.kwargs.get('pk')
-        if pk is None:
-            shipment = ShipmentRetailer.objects.all()
-            if len(shipment) > 0:
-                serializer = ShipmentRetailerSerializer(shipment, many=True)
-                return Response({"error": False, "status_code": 200,"data": serializer.data,"msg": "Data Fetched Successfully"})
-
-            else:
-                token = request.session.get('token')
-                if(token is not None):
-                    try:
-                        kwargs['pageNo'] = "1"
-                        kwargs['token'] = request.session.get('token')
-                        kwargs['client_key'] = request.session.get('client_key')
-                        kwargs['secret_key'] = request.session.get('secret_key')
-                        shipment_key = settings.SHIPMENT_ARR
-                        if len(shipment_key) > 0:
-                            for key in shipment_key:
-                                kwargs['fulfilment_method'] = key
-                                immediate_load.delay(*args, **kwargs)
-                        shipmentArr = getShipment()
-                        return Response(
-                            {"error": False, "status_code": 200,"data": shipmentArr, "msg": "Data Fetched Successfully",})
-                    except Exception as e:
-                        print('e',e)
-                        return Response(
-                        {"error": True, "status_code": 400, "msg": "Internal Server Error",})
-                else:
-                    return Response(
-                            {"error": True, "status_code": 400, "msg": "Token Not Found! Generate using Login.",}
-                        )
-                
-            # the many param informs the serializer that it will be serializing more than a single ShipmentRetailer.
-        else :
-            shipment = ShipmentRetailer.objects.filter(pk=pk)
-            serializer = ShipmentRetailerSerializer(shipment, many=True)
-            return Response({"error": False, "status_code": 200,"data": serializer.data,"msg": "Data Fetched Successfully"})
-            
+class StandardResultsSetPagination(pagination.PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
+class ShipmentView(generics.ListAPIView):
+    queryset = ShipmentRetailer.objects.all()
+    serializer_class = ShipmentRetailerSerializer
+    pagination_class = StandardResultsSetPagination
